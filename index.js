b8df6c51a1065a9d2b05a52326db2fca1db8ec11
Elijah Insua <tmpvar@gmail.com>
2014-05-21 11:53:16 +1000

    remove gl-now add fc

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var glslify = require('glslify');
-var shell = window.shell = require('gl-now')();
 var ndarray = require('ndarray');
 var ndfill = require('ndarray-fill');
 var createTexture = require('gl-texture2d');
 var Vec2 = require('vec2');
+var fc = require('fc');
+var domready = require('domready');
 
 var createRaymarchProgram = glslify({
   vertex: './shaders/raymarch.vert',
@@ -12,17 +13,54 @@ var createRaymarchProgram = glslify({
 
 var cutterRadius = 0.1;
 
-var uniforms = {
-  time : 0,
-  mouse : [0, 0],
-  resolution: [0, 0],
-  cutterRadius : cutterRadius
-};
+var mouse = [0, window.innerHeight];
+document.addEventListener('mousemove', function(ev) {
+  mouse[0] = ev.clientX;
+  mouse[1] = ev.clientY;
+});
 
-shell.on('gl-init', function() {
-  var gl = this.gl;
+var elapsed = 0;
+var start = Date.now(), first = false, v = -0.1;
 
-  this.raymarchProgram = createRaymarchProgram(this.gl);
+// setInterval(function() {
+//   v += .0005;
+// }, 100);
+
+var ratio = (1/2048);
+var r = Math.floor(cutterRadius / ratio);
+
+var tool = ndarray(new Float32Array((r*2 * r*2)), [r*2, r*2]);
+
+ndfill(tool, function(i, j) {
+
+  // x difference, where r is the radius of the tool and i is the x coord
+  var di = (i - r);
+
+  // y difference
+  var dj = (j - r);
+
+  // this is weird because we have to convert the top of the stock,
+  // known to be at 0.05 in the "world"
+  //
+  // basically this gives us the difference between the center of the
+  // ball and the top of the stock
+
+  var dz = Math.sqrt(r * r);
+
+  // compute the distance from 0,0 to x,y
+  var l = Math.sqrt(di * di + dj * dj);
+
+  if (l > r) {
+    return 0;
+  }
+
+  return (Math.sqrt(dz * dz - l * l) / r)  * cutterRadius
+});
+
+domready(function() {
+  var gl = fc(render, true, 3);
+
+  this.raymarchProgram = createRaymarchProgram(gl);
   this.raymarchProgram.bind();
 
   this.buffer = gl.createBuffer();
@@ -51,103 +89,57 @@ shell.on('gl-init', function() {
   this.depthTexture = createTexture(gl, this.depthArray);
 
   this.depthTexture.bind('depth');
-});
+
+  var r2 = r*2;
+  function render(t) {
+
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+
+    //Set viewport
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
 
 
-var mouse = [0, window.innerHeight];
-document.addEventListener('mousemove', function(ev) {
-  mouse[0] = ev.clientX;
-  mouse[1] = ev.clientY;
-});
+    this.raymarchProgram.uniforms.resolution = [
+      gl.drawingBufferWidth,
+      gl.drawingBufferHeight
+    ];
 
-var elapsed = 0;
-var start = Date.now(), first = false, v = -0.1;
+    this.raymarchProgram.uniforms.mouse = mouse;
 
-setInterval(function() {
-  v += .0005;
-}, 100);
+    var time = this.raymarchProgram.uniforms.time = (Date.now() - start)/1000;
 
-var ratio = (1/2048);
-var r = Math.floor(cutterRadius / ratio);
+    var ctime = Math.cos(time)/2;
+    var stime = Math.sin(time)/2;
 
-var tool = ndarray(new Float32Array((r*2 * r*2)), [r*2, r*2]);
+    this.raymarchProgram.uniforms.cutterPosition = [
+      stime/2, ctime/2, -v - cutterRadius
+    ];
 
-ndfill(tool, function(i, j) {
+    this.raymarchProgram.uniforms.cutterRadius = cutterRadius;
 
-  // x difference, where r is the radius of the tool and i is the x coord
-  var di = (i - r);
+    var areax = Math.round((ctime*1024 + 1024));
+    var areay = Math.round((stime*1024 + 1024));
 
-  // y difference
-  var dj = (j - r);
+    var depthArray = this.depthArray.hi(areax+r, areay+r).lo(areax-r, areay-r);
+    v += .0001;
+    for(var i=0; i<r2; ++i) {
+      for(var j=0; j<r2; ++j) {
+        var orig = depthArray.get(i, j);
+        var map = tool.get(i, j);
+        if (map === 0) {
+          continue;
+        }
 
-  // this is weird because we have to convert the top of the stock,
-  // known to be at 0.05 in the "world"
-  //
-  // basically this gives us the difference between the center of the
-  // ball and the top of the stock
+        var computed = v + map;
 
-  var dz = Math.sqrt(r * r);
-
-
-
-  // compute the distance from 0,0 to x,y
-  var l = Math.sqrt(di * di + dj * dj);
-
-  if (l > r) {
-    return 0;
-  }
-
-  return (Math.sqrt(dz * dz - l * l) / r)  * cutterRadius
-});
-
-var r2 = r*2;
-var render = function(t) {
-  var gl = this.gl;
-
-  this.raymarchProgram.uniforms.resolution = [
-    gl.drawingBufferWidth,
-    gl.drawingBufferHeight
-  ];
-
-  this.raymarchProgram.uniforms.mouse = mouse;
-
-  var time = this.raymarchProgram.uniforms.time = (Date.now() - start)/1000;
-
-  var ctime = Math.cos(time)/2;
-  var stime = Math.sin(time)/2;
-
-  this.raymarchProgram.uniforms.cutterPosition = [
-    stime/2, ctime/2, -v - cutterRadius
-  ];
-
-  this.raymarchProgram.uniforms.cutterRadius = cutterRadius;
-
-  var areax = Math.round((ctime*1024 + 1024));
-  var areay = Math.round((stime*1024 + 1024));
-
-  var depthArray = this.depthArray.hi(areax+r, areay+r).lo(areax-r, areay-r);
-
-  for(var i=0; i<r2; ++i) {
-    for(var j=0; j<r2; ++j) {
-      var orig = depthArray.get(i, j);
-
-      var map = tool.get(i, j);
-      if (map === 0) {
-        continue;
-      }
-
-      var computed = v + map;
-
-      if (computed > orig) {
-        depthArray.set(i, j, computed);
+        if (computed > orig) {
+          depthArray.set(i, j, computed);
+        }
       }
     }
+
+    this.depthTexture.setPixels(depthArray, areay - r, areax - r);
+
+    gl.drawArrays(gl.TRIANGLES, 0, 6);
   }
-
-
-  this.depthTexture.setPixels(depthArray, areay - r, areax - r);
-
-  gl.drawArrays(gl.TRIANGLES, 0, 6);
-}
-
-shell.on('gl-render', render);
+});
